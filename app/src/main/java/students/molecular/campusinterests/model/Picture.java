7a8c89b4954e9ef21443ee366b72c37b2fb90557
wallart <julien.wallart@phelsuma.fr>
2016-01-15 11:05:39 +0100

    Add model classes

--- app/src/main/java/students/molecular/campusinterests/model/Picture.java
+++ app/src/main/java/students/molecular/campusinterests/model/Picture.java
@@ -1,33 +1,50 @@
 package students.molecular.campusinterests.model;
 
+import java.io.File;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by meradi on 21/01/16.
  */
 public class Picture {
 
-    private String name;
     private Date date;
-    private Byte[] content;
+    private String url;
+    private File file;
+    private List<HashTag> tags;
+    private User user;
 
-    public  Picture(String name, Byte[] content) {
+    public  Picture(User user, String url, File file, List<HashTag> tags) {
         this.date = new Date();
-        this.content = content;
-        this.name = name;
-    }
+        this.file = file;
+        this.tags = tags;
+        this.user = user;
+        this.url = url;
 
-
-    public String getName() {
-        return name;
     }
 
     public Date getDate() {
         return  date;
     }
 
-    public Byte[] getContent() {
-        return content;
+    public File getFile() {
+        return file;
     }
 
+    public List<HashTag> getTags() {
+        return tags;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
