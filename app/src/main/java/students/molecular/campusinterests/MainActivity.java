5c2d33022e28dcb42b58db6102199a482641092e
wallart <julien.wallart@phelsuma.fr>
2016-01-16 17:43:57 +0100

    Added main view

--- app/src/main/java/students/molecular/campusinterests/MainActivity.java
+++ app/src/main/java/students/molecular/campusinterests/MainActivity.java
@@ -3,11 +3,23 @@ package students.molecular.campusinterests;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
