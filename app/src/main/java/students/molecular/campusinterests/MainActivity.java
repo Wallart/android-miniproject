2cb1c48c3358975a057d323578c4a5f7b5955a5c
wallart <julien.wallart@phelsuma.fr>
2016-01-21 14:19:15 +0100

    Configured Google Maps

--- app/src/main/java/students/molecular/campusinterests/MainActivity.java
+++ app/src/main/java/students/molecular/campusinterests/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -16,10 +17,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        LatLng location = new LatLng(43.5598807, 1.46588);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.addMarker(new MarkerOptions().position(location).title(getString(R.string.college_name)));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 17.0f));
     }
 }
